c913aef56f19bdb23774b730e8a7b1fdb15edcfa
Yasuhisa Yoshida <syou6162@gmail.com>
2015-11-27 16:32:22 +0900

    added project.clj

---
+++ project.clj
@@ -0,0 +1,7 @@
+(defproject crfpp-template-gen "0.0.1"
+  :description "Simple DSL for CRF++"
+  :url "https://github.com/syou6162/crfpp-template-gen"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.6.0"]]
+  :main crfpp-template-gen.core)
